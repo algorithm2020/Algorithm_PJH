b37a2fb306973ed0021f123f2c08fe575a5cccea
hoon <wlgns3737@gmail.com>
2020-02-18 23:07:49 +0900

    알파벳

--- backtracking_tree/boj_1987.cpp
+++ backtracking_tree/boj_1987.cpp
@@ -1,69 +1,45 @@
 #include <iostream>
-#include <vector>
+#include <string>
 
-int r, c; 
-char arr[20][20];
-bool visited[20][20] = {false,};
-bool checked[26] = {false,};
-int dir[4][2] = {{-1,0},{0,1},{1,0},{0,-1}};
-int res =0;
+int r, c;
+char arr[21][21];
+bool checked[26] = { false, };
+int dir[4][2] = { { -1,0 },{ 0,1 },{ 1,0 },{ 0,-1 } };
+int res = 0;
 
 bool isRanged(int y, int x) {
-	return ((y >= 0 && y < r) && (x >= 0 && x < c));
+	return (y >= 0 && y < r && x >= 0 && x < c);
 }
 
-int toInt(char a){
-    return (int)a - 'A';
+int toInt(char a) {
+	return (int)a - 'A';
 }
 
-void print(int y, int x){
-    for(int i =0; i< r; i++){
-        for(int j =0; j< c; j++){
-            std::cout << visited[i][j];
-        }
-        std::cout << std::endl;
-    }
-    std::cout << std::endl;
+void dfs(int curY, int curX, int cnt) {
+	for (int i = 0; i < 4; i++) {
+		int nextY = curY + dir[i][0];
+		int nextX = curX + dir[i][1];
 
-    for(int i =0; i< 26; i++){
-        std::cout << checked[i];
-    }
-
-    std::cout << std::endl;
-    std::cout << y << x << std::endl;
+		if (isRanged(nextY, nextX) && !checked[toInt(arr[nextY][nextX])]) {
+			checked[toInt(arr[nextY][nextX])] = true;
+			dfs(nextY, nextX, cnt + 1);
+			checked[toInt(arr[nextY][nextX])] = false;
+		}
+	}
+	res = (res < cnt) ? cnt : res;
 }
 
-void dfs(int curY, int curX, int cnt){
-    if(checked[toInt(arr[curY][curX])]){
-        res = (res < cnt ) ? cnt : res;
-        return;
-    }
-    checked[toInt(arr[curY][curX])] = true;
-    for(int i =0; i<4; i++){
-        int nextY = curY + dir[i][0];
-        int nextX = curX + dir[i][1];
+int main() {
 
-        if(isRanged(nextY, nextY) && !visited[nextY][nextX]){
-            visited[nextY][nextX] = true;
-            print(nextY, nextX);
-            dfs(nextY, nextX, cnt + 1 );
-            visited[nextY][nextX] = false;
-        }
-    }
-    checked[toInt(arr[curY][curX])] = false;
-}
+	std::cin >> r >> c;
 
-int main(){
-    
-    std::cin >> r >> c;
-
-    for(int i =0; i< r; i++){
-        for(int j =0; j< c; j++){
-            std::cin >> arr[i][j];
-        }
-    }
-    visited[0][0] = true;
-    dfs(0,0,0);
-    std::cout << res;
-    return 0;
+	for (int i = 0; i< r; i++) {
+		for (int j = 0; j< c; j++) {
+			std::cin >> arr[i][j];
+		}
+	}
+	checked[toInt(arr[0][0])] = true;
+	dfs(0, 0, 1);
+	std::cout << res;
+	return 0;
 }
